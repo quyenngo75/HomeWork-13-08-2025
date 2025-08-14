8eaaa2ec0ebd33ec10b5741b7e2a1e7eeecc6617
quyenngo75 <bonclay205@gmail.com>
2025-08-14 09:16:38 +0700

    init project

---
+++ HomeWork.java
@@ -0,0 +1,22 @@
+package HomeWork;
+
+public class HomeWork {
+     public static void main(String[] args)
+     {
+         int a = 9 / 5;
+         int b = 695 % 20;
+         int c = 7 + 6 * 5;
+         int d = 248 % 100 / 5;
+         int e = 6 * 3 - 9 / 4;
+         int f = (5 - 7) * 4;
+         int g = 6 + (18 % (17 - 12)); 
+         System.out.println("9/5 = " +a);
+         System.out.println("695 % 20 = " +b);
+         System.out.println("7 + 6 * 5= " +c);
+         System.out.println("248 % 100 / 5 = " +d);
+         System.out.println("6 * 3 - 9 / 4= " +e);
+         System.out.println("(5 - 7) * 4 = " +f);
+         System.out.println("6 + (18 % (17 - 12)) = " +g);
+         
+     }
+}
